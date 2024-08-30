d5b282e10ac1245052ee19605603533d880f4450
talha105 <talhakhan105@gmail.com>
2024-08-30 12:54:45 +0500

    back to old

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -232,38 +232,11 @@ class AuthController extends Controller
             
             if($type == "seller"){
                 // seller registered
-                if($request->image){
-                    $image = $request->image;
-                    $logo =  rand(99, 9999) . time() . $image->getClientOriginalName();
-                    $image->storeAs('public/sellers', $logo);
-                }
-
-                if ($request->hasFile('docs')) {
-                    $docs = $request->file('docs');
-                    $documents = []; // Array to hold the names of the saved documents
-                
-                    foreach ($docs as $doc) {
-                        // Generate a unique name for each document
-                        $documentName = rand(99, 9999) . time() . $doc->getClientOriginalName();
-                        // Store the document in the specified directory
-                        $doc->storeAs('public/sellers', $documentName);
-                        // Add the document name to the array
-                        $documents[] = $documentName;
-                    }
-                    
-                    // Convert the array of document names to a comma-separated string (or another format if needed)
-                    $documentsString = implode(',', $documents) ?? NULL;
-                } else {
-                    $documentsString = NULL; // No documents were uploaded
-                }
-                
-
-                $seller = Seller::create([
-                'garage_name' => $request->garage_name,,
+                            $seller = Seller::create([
                 'user_id' => $user->id,
                 'city' => $request->city ?? "Null",
                 'about' => $request->about ?? "Nothing",
-                'docs' => $documentsString ?? "Null",
+                'docs' => isset($docs) ? $docs : NULL,
                 'logo' => isset($logo) ? $logo : NULL,
                 ]);
             }
