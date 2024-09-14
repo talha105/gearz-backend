9c85e4407bc5cb4e5d20eae6eee6f57ee8001043
talha105 <talhakhan105@gmail.com>
2024-09-14 13:27:10 +0500

    add model

---
+++ app/Subscription.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Subscription extends Model
+{
+    protected $guarded = ['id'];
+    protected $appends = ['create_date'];
+
+    public function getCreateDateAttribute(){
+        return $this->created_at ? $this->created_at->toDateString() : "";
+    }
+}
